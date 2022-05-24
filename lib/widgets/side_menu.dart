5157848a644ab3e6d06e10090f2a7293a97b4679
rieadaits <rieadaits21@gmail.com>
2022-05-24 15:18:44 +0600

    sidebar playlist item added

--- lib/widgets/side_menu.dart
+++ lib/widgets/side_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spotify_ui/data/data.dart';
 
 class SideMenu extends StatelessWidget {
   const SideMenu({Key? key}) : super(key: key);
@@ -8,7 +9,142 @@ class SideMenu extends StatelessWidget {
     return Container(
       height: double.infinity,
       width: 280,
-      color: Colors.green,
+      color: Theme.of(context).primaryColor,
+      child: Column(
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset(
+                'assets/spotify_logo.png',
+                height: 55,
+                filterQuality: FilterQuality.high,
+              ),
+            ),
+          ),
+          _SideMenuIconTab(
+            iconData: Icons.home,
+            title: 'Home',
+            onTap: () {},
+          ),
+          _SideMenuIconTab(
+            iconData: Icons.search,
+            title: 'Search',
+            onTap: () {},
+          ),
+          _SideMenuIconTab(
+            iconData: Icons.audiotrack,
+            title: 'Radio',
+            onTap: () {},
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          _LibraryPlaylists(),
+        ],
+      ),
+    );
+  }
+}
+
+class _SideMenuIconTab extends StatelessWidget {
+  final IconData iconData;
+  final String title;
+  final VoidCallback onTap;
+
+  const _SideMenuIconTab({
+    Key? key,
+    required this.iconData,
+    required this.title,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(
+        iconData,
+        color: Theme.of(context).iconTheme.color,
+        size: 28,
+      ),
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.bodyText1,
+        overflow: TextOverflow.ellipsis,
+      ),
+      onTap: onTap,
+    );
+  }
+}
+
+class _LibraryPlaylists extends StatefulWidget {
+  const _LibraryPlaylists({Key? key}) : super(key: key);
+
+  @override
+  State<_LibraryPlaylists> createState() => _LibraryPlaylistsState();
+}
+
+class _LibraryPlaylistsState extends State<_LibraryPlaylists> {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView(
+        physics: ClampingScrollPhysics(),
+        padding: EdgeInsets.symmetric(vertical: 12),
+        children: [
+          _PlaylistItem(
+            title: 'Your Library',
+            itemList: yourLibrary,
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          _PlaylistItem(
+            title: 'Playlists',
+            itemList: playlists,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _PlaylistItem extends StatelessWidget {
+  final String title;
+  final List<String> itemList;
+
+  const _PlaylistItem({Key? key, required this.title, required this.itemList})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 8,
+            horizontal: 8,
+          ),
+          child: Text(
+            title,
+            style: Theme.of(context).textTheme.headline4,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+        ...itemList
+            .map(
+              (e) => ListTile(
+                title: Text(
+                  e,
+                  style: Theme.of(context).textTheme.bodyText2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            )
+            .toList()
+      ],
     );
   }
 }
